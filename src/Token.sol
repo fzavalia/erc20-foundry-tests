513bea09c0e17dab613953511b6ec6b60fcca2ad
Nando <24811313+fzavalia@users.noreply.github.com>
2023-01-21 13:42:56 -0300

    feat: Mint 21 mill to sender

--- src/Token.sol
+++ src/Token.sol
@@ -4,5 +4,7 @@ pragma solidity ^0.8.13;
 import "openzeppelin-contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20 {
-    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {}
+    constructor(string memory _name, string memory _symbol) ERC20(_name, _symbol) {
+        _mint(_msgSender(), 21_000_000 ether);
+    }
 }
